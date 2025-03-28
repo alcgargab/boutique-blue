8f58642cbe77758fa53cd7e848ea54f911a8aab5
GabrielAlcarazSX <gabriel.alcaraz@strategix.com.mx>
2025-03-28 01:01:45 -0600

    v1.1

--- app/Controllers/Blue.php
+++ app/Controllers/Blue.php
@@ -8,6 +8,7 @@
 
         public function index(): string
         {
+            // echo "<pre>"; print_r('hola'); echo "</pre>"; die();
             return view('header')
                 .view('dashboard')
                 .view('footer');
